74c510fb8608f8411252316219d869a769857b54
luis.manzanaro@gmail.com <luis.manzanaro@gmail.com>
2022-11-26 11:51:24 +0100

    Initial commit

---
+++ src/test/kotlin/com/soma/bookstore/historical/HistoricalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.soma.bookstore.historical
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HistoricalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
